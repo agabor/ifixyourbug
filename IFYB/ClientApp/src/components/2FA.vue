c94a90ea5b66b3e0756f7fb8d7b2607d696542bf
Lívia Orsós <livia.orsos@codesharp.hu>
2022-05-20 10:24:08 +0200

    #IFYB-12 2fa into a separate component

---
+++ IFYB/ClientApp/src/components/2FA.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="carousel-item">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-md-7 mx-auto">
+          <div class="card">
+            <div class="card-body px-lg-5 py-lg-5 text-center">
+              <div class="info mb-4">
+                <div class="icon icon-shape icon-xl rounded-circle bg-gradient-primary shadow text-center py-3 mx-auto">
+                  <i class="ni ni-atom opacity-10 mt-2"></i>
+                </div>
+              </div>
+              <h2>2FA Security</h2>
+              <p class="mb-4">Enter 6-digits code from your athenticatior app.</p>
+              <div class="row mb-4">
+                <div class="col-lg-2 col-md-2 col-2 ps-0 ps-md-2" v-for="(i, idx) in 6" :key="i">
+                  <input type="text" :id="`2fa-${idx}`" class="form-control text-lg text-center" @keyup.enter="checkValidCode" @keyup.delete="deleteFromAuth(idx)" v-model="auth[idx]" aria-label="2fa">
+                </div>
+              </div>
+              <div class="alert alert-warning text-white font-weight-bold" role="alert" v-if="codeError">
+                {{codeError}}
+              </div>
+              <div class="alert alert-warning text-white font-weight-bold" role="alert" v-else-if="error">
+                {{error}}
+              </div>
+              <div class="text-center">
+                <button type="button" id="2fa-btn" class="btn bg-gradient-primary my-4" @click="checkValidCode">Check</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+import { required, min } from '../utils/Validate';
+
+export default {
+  name: '2FA',
+  props: {
+    modelValue: String,
+    error: String
+  },
+  emits:['update:modelValue'],
+  setup(props, context) {
+    const auth = ref(props.modelValue.split(''));
+    let authLength = 6;
+    const codeError = ref(null);
+    
+    watch(auth.value, () => {
+      for(let i = 0; i < authLength; i++) {
+        if(auth.value[i] && auth.value[i].length > 1) {
+          let code = auth.value[i];
+          for(let j = 0; j < code.length; j++){
+            if(i+j < authLength) {
+              auth.value[i+j] = code[j];
+            } else {
+              break;
+            }
+          }
+        }
+      }
+
+      let focused = false;
+      for(let i = 0; i < authLength; i++) {
+        if(auth.value[i] === '' || auth.value[i] === undefined) {
+          document.getElementById('2fa-' + i).focus();
+          focused = true;
+          break;
+        }
+      }
+      if(!focused) {
+        checkValidCode();
+      }
+    })
+    
+    function deleteFromAuth(idx) {
+      if(idx - 1 > -1 && (auth.value[idx] === '' || auth.value[idx] === undefined)) { 
+        auth.value[idx - 1] = ''
+      }
+    }
+
+    function checkValidCode() {
+      let err = required(auth.value, 'Authentication code', '2fa-0')?.message || min(auth.value.join(''), 6);
+      if(err) {
+        codeError.value = err;
+      } else {
+        codeError.value = null;
+        context.emit('update:modelValue', auth.value.join(''));
+      }
+    }
+
+    return { auth, codeError, deleteFromAuth, checkValidCode }
+  }
+}
+</script>
--- IFYB/ClientApp/src/views/AdminLogin.vue
+++ IFYB/ClientApp/src/views/AdminLogin.vue
@@ -31,102 +31,27 @@
             </div>
           </div>
         </div>
-        <div class="carousel-item" :class="{'active': page === 'auth'}">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5 col-md-7 mx-auto">
-                <div class="card">
-                  <div class="card-body px-lg-5 py-lg-5 text-center">
-                    <div class="info mb-4">
-                      <div class="icon icon-shape icon-xl rounded-circle bg-gradient-primary shadow text-center py-3 mx-auto">
-                        <i class="ni ni-atom opacity-10 mt-2"></i>
-                      </div>
-                    </div>
-                    <h2>2FA Security</h2>
-                    <p class="mb-4">Enter 6-digits code from your athenticatior app.</p>
-                    <div class="row mb-4">
-                      <div class="col-lg-2 col-md-2 col-2 ps-0 ps-md-2">
-                        <input type="text" id="2fa-0" class="form-control text-lg text-center" @keyup.enter="checkAuthentication()" @keyup.delete="deleteFromAuth(0)" v-model="auth[0]" aria-label="2fa">
-                      </div>
-                      <div class="col-lg-2 col-md-2 col-2 ps-0 ps-md-2">
-                        <input type="text" id="2fa-1" class="form-control text-lg text-center" @keyup.enter="checkAuthentication()" @keyup.delete="deleteFromAuth(1)" v-model="auth[1]" aria-label="2fa">
-                      </div>
-                      <div class="col-lg-2 col-md-2 col-2 ps-0 ps-md-2">
-                        <input type="text" id="2fa-2" class="form-control text-lg text-center" @keyup.enter="checkAuthentication()" @keyup.delete="deleteFromAuth(2)" v-model="auth[2]" aria-label="2fa">
-                      </div>
-                      <div class="col-lg-2 col-md-2 col-2 pe-0 pe-md-2">
-                        <input type="text" id="2fa-3" class="form-control text-lg text-center" @keyup.enter="checkAuthentication()" @keyup.delete="deleteFromAuth(3)" v-model="auth[3]" aria-label="2fa">
-                      </div>
-                      <div class="col-lg-2 col-md-2 col-2 pe-0 pe-md-2">
-                        <input type="text" id="2fa-4" class="form-control text-lg text-center" @keyup.enter="checkAuthentication()" @keyup.delete="deleteFromAuth(4)" v-model="auth[4]" aria-label="2fa">
-                      </div>
-                      <div class="col-lg-2 col-md-2 col-2 pe-0 pe-md-2">
-                        <input type="text" id="2fa-5" class="form-control text-lg text-center" @keyup.enter="checkAuthentication()" @keyup.delete="deleteFromAuth(5)" v-model="auth[5]" aria-label="2fa">
-                      </div>
-                    </div>
-                    <div class="alert alert-warning text-white font-weight-bold" role="alert" v-if="error">
-                      {{error}}
-                    </div>
-                    <div class="text-center">
-                      <button type="button" id="2fa-btn" class="btn bg-gradient-primary my-4" @click="checkAuthentication()">Check</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <two-fa :class="{'active': page === 'auth'}" :error="error" :modelValue="auth" @update:modelValue="checkAuthentication"></two-fa>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import { ref, watch } from 'vue';
-import { validEmail, required, min } from '../utils/Validate';
+import { ref } from 'vue';
+import { validEmail } from '../utils/Validate';
+import TwoFa from '../components/2FA.vue';
 export default {
   name: 'AdminLogin',
+  components: { TwoFa },
   setup() {
     const page = ref('email');
     const order = ref({});
     const error = ref(null);
-    const auth = ref([])
+    const auth = ref('');
     let clientId;
     let jwt;
-    let authLength = 6;
     
-    watch(auth.value, () => {
-      for(let i = 0; i < authLength; i++) {
-        if(auth.value[i] && auth.value[i].length > 1) {
-          let code = auth.value[i];
-          for(let j = 0; j < code.length; j++){
-            if(i+j < authLength) {
-              auth.value[i+j] = code[j];
-            } else {
-              break;
-            }
-          }
-        }
-      }
-
-      let focused = false;
-      for(let i = 0; i < authLength; i++) {
-        if(auth.value[i] === '' || auth.value[i] === undefined) {
-          document.getElementById('2fa-' + i).focus();
-          focused = true;
-          break;
-        }
-      }
-      if(!focused) {
-        checkAuthentication();
-      }
-    })
-    
-    function deleteFromAuth(idx) {
-      if(idx - 1 > -1 && (auth.value[idx] === '' || auth.value[idx] === undefined)) { 
-        auth.value[idx - 1] = ''
-      }
-    }
     async function submitEmail() {
       let err = validEmail(order.value.email);
       if(err) {
@@ -151,26 +76,21 @@ export default {
       }
     }
 
-    async function checkAuthentication() {
-      let err = required(auth.value.join(''), 'Authentication code', '2fa-0')?.message;
-      if(!err)
-        err = min(auth.value.join(''), 6);
-      if(err) {
-        error.value = err;
-      } else {
-        try {
-          error.value = null;
-          const response = await fetch(`authenticate/admin/${clientId}`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({'clientId': clientId, 'password': auth.value.join('')})
-          });
-          jwt = (await response.json()).jwt;
-        } catch(e) {
-          error.value = 'Wrong code.';
-        }
+    async function checkAuthentication(code) {
+      auth.value = code;
+      try {
+        error.value = null;
+        const response = await fetch(`authenticate/admin/${clientId}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({'clientId': clientId, 'password': auth.value})
+        });
+        jwt = (await response.json()).jwt;
+      } catch(e) {
+        jwt = null;
+        error.value = 'Wrong code.';
       }
       if(jwt) {
         let adminResponse = await fetch('/admin/contact-messages', {
@@ -183,7 +103,7 @@ export default {
         console.log('adminResponse', await adminResponse.json());
       }
     }
-    return { page, error, order, auth, submitEmail, checkAuthentication, deleteFromAuth }
+    return { page, error, order, auth, submitEmail, checkAuthentication }
   }
 }
 </script>
