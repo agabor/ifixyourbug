3eb488401d2772e832b707fa93c788ecded0ba0b
Lívia Orsós <livia.orsos@codesharp.hu>
2022-06-01 14:45:54 +0200

    #IFYB-30 refactor order view

--- IFYB/ClientApp/src/views/OrderView.vue
+++ IFYB/ClientApp/src/views/OrderView.vue
@@ -3,18 +3,18 @@
     <div id="carousel-testimonials" class="page-header min-vh-100">
       <span class="mask bg-gradient-dark opacity-4"></span>
       <div class="carousel-inner">
-        <carousel-item :class="{'active': page === 'email'}" icon="email-83" :title="$t('order.email')" :subTitle="$t('order.emailDes')" :buttonText="$t('order.submit')" :error="error" @onClickBtn="submitEmail()">
+        <carousel-item :class="{'active': page === 'email'}" icon="email-83" :title="$t('order.email')" :subTitle="$t('order.emailDes')" :buttonText="$t('order.submit')" :error="error" @onClickBtn="trySubmitEmail()">
           <div class="row mb-4">
-            <input id="emailInput" class="form-control" :placeholder="$t('order.emailExample')" type="email" @keyup.enter="submitEmail()" v-model="order.email">
+            <input id="emailInput" class="form-control" :placeholder="$t('order.emailExample')" type="email" @keyup.enter="trySubmitEmail()" v-model="order.email">
           </div>
         </carousel-item>
         <two-fa :class="{'active': page === 'auth'}" :error="error" :modelValue="auth" @update:modelValue="checkAuthentication"></two-fa>
-        <carousel-item :class="{'active': page === 'name'}" icon="badge" :title="$t('order.name')" :subTitle="$t('order.nameDes')" :buttonText="$t('order.save')" :error="error" @onClickBtn="setName()">
+        <carousel-item :class="{'active': page === 'name'}" icon="badge" :title="$t('order.name')" :subTitle="$t('order.nameDes')" :buttonText="$t('order.save')" :error="error" @onClickBtn="trySetName()">
           <div class="row mb-4">
-            <input id="name-input" class="form-control" placeholder="Your Name" type="text" @keyup.enter="setName()" v-model="order.name">
+            <input id="name-input" class="form-control" placeholder="Your Name" type="text" @keyup.enter="trySetName()" v-model="order.name">
           </div>
         </carousel-item>
-        <carousel-item class="full-height" :class="{'active': page === 'data'}" width="col-lg-9 col-md-11" icon="spaceship" :title="$t('order.orderData')" :subTitle="$t('order.orderDataDes')" :buttonText="$t('order.submit')" :error="error" @onClickBtn="submitOrder()">
+        <carousel-item class="full-height" :class="{'active': page === 'data'}" width="col-lg-9 col-md-11" icon="spaceship" :title="$t('order.orderData')" :subTitle="$t('order.orderDataDes')" :buttonText="$t('order.submit')" :error="error" @onClickBtn="trysubmitOrder()">
           <form>
             <div class="row text-start">
               <div class="col-md-12 d-flex pe-2 mb-3">
@@ -92,9 +92,9 @@ export default {
       }
     })
 
-    checkJwtIsActive();
+    setJwtIfActive();
 
-    async function checkJwtIsActive() {
+    async function setJwtIfActive() {
       if(localStorage.getItem('jwt')) {
         let authResponse = await fetch('/authenticate/check-jwt', {
           method: 'GET',
@@ -109,105 +109,129 @@ export default {
       }
     }
     
-    async function submitEmail() {
+    function trySubmitEmail() {
       let err = validEmail(order.value.email);
       if(err) {
         error.value = tm(err);
       } else {
-        const response = await fetch('/authenticate', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({'email': order.value.email})
-        });
-        clientId = (await response.json()).id;
-        page.value = 'auth';
-        error.value = null;
+        submitEmail();
       }
+    }
+
+    async function submitEmail() {
+      const response = await fetch('/authenticate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({'email': order.value.email})
+      });
+      clientId = (await response.json()).id;
+      page.value = 'auth';
+      error.value = null;
     }
 
     async function checkAuthentication(code) {
       auth.value = code;
       try {
-        const response = await fetch(`/authenticate/${clientId}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({'clientId': clientId, 'password': auth.value})
-        });
-        jwt = (await response.json()).jwt;
-        localStorage.setItem('jwt', jwt);
-        error.value = null;
+        await tryAuthenticate();
       } catch(e) {
-        jwt = null;
-        error.value = tm('errors.wrongCode');
+        handleAuthenticateError();
       }
       if(jwt) {
         toNamePageOrToDataPage();
       }
     }
 
-    async function setName() {
-      let err = required(order.value.name, 'Name', 'name-input');
+    async function tryAuthenticate() {
+      const response = await fetch(`/authenticate/${clientId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({'clientId': clientId, 'password': auth.value})
+      });
+      jwt = (await response.json()).jwt;
+      localStorage.setItem('jwt', jwt);
+      error.value = null;
+    }
+
+    function handleAuthenticateError() {
+      jwt = null;
+      error.value = tm('errors.wrongCode');
+    }
+
+    function trySetName() {
+      let err = required(order.value.name, tm('errors.requiredName'), 'name-input');
       if(err) {
         error.value = err;
       } else {
-        await fetch('/clients/name', {
-          method: 'POST',
-          headers: {
-            'Authorization': `bearer ${jwt}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({'name': order.value.name})
-        });
-        page.value = 'data';
-        error.value = null;
+        setName();
       }
     }
 
-    async function submitOrder() {
+    async function setName() {
+      await fetch('/clients/name', {
+        method: 'POST',
+        headers: {
+          'Authorization': `bearer ${jwt}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({'name': order.value.name})
+      });
+      page.value = 'data';
+      error.value = null;
+    }
+
+    function trysubmitOrder() {
       let err = getOrderFormError();        
       if(err) {
         error.value = err;
       } else {
-        let gitAccessId;
-        if(selectedAccess.value.id != undefined){
-          gitAccessId = selectedAccess.value.id;
-        } else {
-          let gitResponse = await fetch('/git-accesses', {
-            method: 'POST',
-            headers: {
-              'Authorization': `bearer ${jwt}`,
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({'url': order.value.repoUrl, 'accessMode': order.value.accessMode})
-          });
-          gitAccessId = (await gitResponse.json()).id;
-        }
-        let orderResponse = await fetch('/orders', {
+        submitOrder();
+      }
+    }
+
+    async function submitOrder() {
+      let orderResponse = await fetch('/orders', {
+        method: 'POST',
+        headers: {
+          'Authorization': `bearer ${jwt}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'framework': order.value.framework,
+          'version': order.value.version,
+          'thirdPartyTool': order.value.isThirdPartyTool ? order.value.thirdPartyTool : '',
+          'projectDescription': order.value.projectDescription,
+          'bugDescription': order.value.bugDescription,
+          'gitAccessId': await getGitAccessId()
+        })
+      });
+      if(orderResponse.status == 200) {
+        page.value = 'success';
+        error.value = null;
+      } else {
+        error.value = `${tm('errors.somethingWrong')} - ${orderResponse.statusText} (${orderResponse.status})`;
+      }
+    }
+
+    async function getGitAccessId() {
+      let gitAccessId;
+      if(selectedAccess.value.id != undefined){
+        gitAccessId = selectedAccess.value.id;
+      } else {
+        let gitResponse = await fetch('/git-accesses', {
           method: 'POST',
           headers: {
             'Authorization': `bearer ${jwt}`,
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({
-            'framework': order.value.framework,
-            'version': order.value.version,
-            'thirdPartyTool': order.value.isThirdPartyTool ? order.value.thirdPartyTool : '',
-            'projectDescription': order.value.projectDescription,
-            'bugDescription': order.value.bugDescription,
-            'gitAccessId': gitAccessId
-          })
+          body: JSON.stringify({'url': order.value.repoUrl, 'accessMode': order.value.accessMode})
         });
-        if(orderResponse.status == 200) {
-          page.value = 'success';
-          error.value = null;
-        } else {
-          error.value = `${tm('errors.somethingWrong')} - ${orderResponse.statusText} (${orderResponse.status})`;
-        }
+        gitAccessId = (await gitResponse.json()).id;
       }
+      return gitAccessId;
     }
 
     async function toNamePageOrToDataPage() {
@@ -267,7 +291,7 @@ export default {
       order.value.browserVersion = undefined;
     }
 
-    return { page, error, order, auth, aspVersions, vueVersions, gitAccesses, selectedAccess, submitEmail, checkAuthentication, setName, submitOrder, changeFramework }
+    return { page, error, order, auth, aspVersions, vueVersions, gitAccesses, selectedAccess, trySubmitEmail, checkAuthentication, trySetName, trysubmitOrder, changeFramework }
   }
 }
 </script>
