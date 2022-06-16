c4dbaaf21ce92f596134a3fba49395bddc9cce21
Lívia Orsós <livia.orsos@codesharp.hu>
2022-06-16 15:43:29 +0200

    #IFYB-45

--- IFYB/ClientApp/src/views/FAQ.vue
+++ IFYB/ClientApp/src/views/FAQ.vue
@@ -1,4 +1,6 @@
 <template>
+<span class="bg"></span>
+<span class="mask bg-gradient-dark opacity-4"></span>
   <section class="pt-3 pt-md-5 pt-lg-7 pb-md-5 pb-lg-7">
     <div class="container">
       <div class="col-lg-8 col-md-10 mx-auto">
@@ -94,3 +96,15 @@ export default {
   }
 }
 </script>
+<style scoped>
+.bg {
+  position: absolute;
+  background-size: cover;
+  background-position: center center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('../assets/img/pricing3.jpg');
+}
+</style>
