75e741bd5898b76a8fed0f61c9746b9145a143d0
Lívia Orsós <livia.orsos@codesharp.hu>
2022-07-18 15:46:19 +0200

    #IFYB-90 progress to pay button

--- IFYB/ClientApp/src/views/CheckoutView.vue
+++ IFYB/ClientApp/src/views/CheckoutView.vue
@@ -10,6 +10,9 @@
             <form :action="`/api/pay/${route.params.token}`" method="post">
                 <a class="btn btn bg-gradient-primary btn-round" @click="pay">Pay</a>
             </form>
+            <div class="progress">
+              <div class="progress-bar bg-primary" role="progressbar" :style="`width: ${progress}%`" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
           </div>
           <p v-if="!order && !loading">{{ $t('checkout.checkoutLink') }}</p>
         </carousel-item>
@@ -31,21 +34,31 @@ export default {
     const order = ref(null);
     const route = useRoute();
     const payment = usePayment();
+    const progress = ref(0);
+
     fetch(`/api/pay/${route.params.token}`).then(resp => {
-        resp.json().then(data => {
-            order.value = data;
-            loading.value = false;
-        });
+      resp.json().then(data => {
+        order.value = data;
+        loading.value = false;
+      });
     });
     function pay() {
-        fetch(`/api/pay/${route.params.token}`, { method: 'post' }).then(resp => {
-            payment.setPaymentToken(route.params.token)
-            resp.json().then(data => {
-                window.location.href = data.url
-            });
+      progress.value = 30;
+      fetch(`/api/pay/${route.params.token}`, { method: 'post' }).then(resp => {
+        payment.setPaymentToken(route.params.token)
+        resp.json().then(data => {
+          window.location.href = data.url
         });
+        progress.value = 100;
+      });
     }
-    return { loading, order, route, pay };
+    return { loading, order, route, progress, pay };
   }
 }
 </script>
+
+<style scoped>
+  .progress {
+    background-color: transparent;
+  }
+</style>
