ebc19f9346aaec39407eaf7e35193866727c00f6
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-07-18 21:32:59 +0200

    deployment

--- IFYB/deploy.sh
+++ IFYB/deploy.sh
@@ -6,4 +6,6 @@ cd bin/Release/net6.0/publish
 zip -r ifyb.zip .
 cd ../../../..
 scp -i deploy.pem bin/Release/net6.0/publish/ifyb.zip admin@ifixyourbug.com:/home/admin/ifyb.zip
+scp -i deploy.pem setup.sh admin@ifixyourbug.com:/home/admin/setup.sh
+ssh -i deploy.pem admin@ifixyourbug.com chmod +x /home/admin/setup.sh
 ssh -i deploy.pem admin@ifixyourbug.com /home/admin/setup.sh
