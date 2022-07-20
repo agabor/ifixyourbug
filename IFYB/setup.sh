b565ee9bfc119184adadde29a2bb7442f82512b9
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-07-20 15:23:24 +0200

    update setup script

--- IFYB/setup.sh
+++ IFYB/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo systemctl stop kestrel-ifyb.service
-rm -rf /var/www/ifyb
+rm -rf /var/www/ifyb/*
 unzip ifyb.zip -d /var/www/ifyb
 cp appsettings.json /var/www/ifyb/appsettings.json
 sudo systemctl start kestrel-ifyb.service
