b6558e63212498843c31557c8f100224a08faf19
Lívia Orsós <livia.orsos@codesharp.hu>
2022-09-08 09:33:56 +0200

    #IFYB-139 set uploded image folder

--- IFYB/Options/AppOptions.cs
+++ IFYB/Options/AppOptions.cs
@@ -5,5 +5,6 @@ public class AppOptions
     public const string Host = "Host";
     public string BaseUrl { get; set; } = String.Empty;
     public bool SendEmails { get; set; } = false;
+    public string ImgFolder { get; set; } = String.Empty;
 
 }
