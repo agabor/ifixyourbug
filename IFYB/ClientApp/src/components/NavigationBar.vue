8f33849816ef65c77b8623926e9155f14e15724f
Lívia Orsós <livia.orsos@codesharp.hu>
2022-09-13 16:24:41 +0200

    #IFYB-146 remove order from localStorage on logout

--- IFYB/ClientApp/src/components/NavigationBar.vue
+++ IFYB/ClientApp/src/components/NavigationBar.vue
@@ -78,6 +78,7 @@ export default {
       adminAuth.setJwt(null);
       userAuth.requestedPage.value = null;
       adminAuth.requestedPage.value = null;
+      localStorage.removeItem('order');
       userAuth.resetUserData();
       router.push('/');
     }
