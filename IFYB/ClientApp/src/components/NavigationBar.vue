a60176f967ecf5fc26c01cda6a7a1575adefd72e
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-09-20 11:44:31 +0200

    seo

--- IFYB/ClientApp/src/components/NavigationBar.vue
+++ IFYB/ClientApp/src/components/NavigationBar.vue
@@ -1,11 +1,16 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-white top-0 z-index-3 shadow position-fixed py-2 start-0 end-0 row px-lg-6 px-4">
     <div class="container px-0">
-      <a class="navbar-brand font-weight-bolder cursor-pointer ms-sm-3" @click="isAdminLoggedIn ? $router.push('/admin') : $router.push('/')" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
-        {{ $t('navigationBar.projectName') }}
-      </a>
-      <a class="btn btn-sm bg-gradient-primary btn-round mb-0 mb-0 ms-auto d-lg-none d-block" @click="toLogin" v-if="!isLoggedIn">{{ $t('navigationBar.login') }}</a>
-      <a class="btn btn-sm bg-gradient-primary btn-round mb-0 mb-0 ms-auto d-lg-none d-block" @click="logout" v-else>{{ $t('navigationBar.logout') }}</a>
+
+      <router-link to="/">
+        <a class="navbar-brand font-weight-bolder cursor-pointer ms-sm-3" href="/" >
+          {{ $t('navigationBar.projectName') }}
+        </a>
+      </router-link>
+      <router-link to="/authentication">
+        <a class="btn btn-sm bg-gradient-primary btn-round mb-0 mb-0 ms-auto d-lg-none d-block" href="/authentication"  v-if="!isLoggedIn">{{ $t('navigationBar.login') }}</a>
+      </router-link>
+      <a class="btn btn-sm bg-gradient-primary btn-round mb-0 mb-0 ms-auto d-lg-none d-block" @click="logout" v-if="isLoggedIn">{{ $t('navigationBar.logout') }}</a>
       <button class="navbar-toggler shadow-none ms-md-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
           <span class="navbar-toggler-bar bar1"></span>
@@ -28,9 +33,11 @@
         </ul>
         <ul class="navbar-nav navbar-nav-hover me-auto" v-else>
           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" @click="$router.push('/faq')">
-              {{ $t('navigationBar.faq') }}
-            </a>
+            <router-link to="/faq">
+              <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" href="/faq">
+                {{ $t('navigationBar.faq') }}
+              </a>
+            </router-link>
           </li>
           <li class="nav-item dropdown dropdown-hover mx-2" v-if="isUserLoggedIn">
             <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" @click="$router.push('/my-orders')">
@@ -48,8 +55,10 @@
         </ul>
         <ul class="navbar-nav d-lg-block d-none">
           <li class="nav-item">
-            <a class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1" @click="toLogin" v-if="!isLoggedIn">{{ $t('navigationBar.login') }}</a>
-            <a class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1" @click="logout" v-else>{{ $t('navigationBar.logout') }}</a>
+            <router-link to="/authentication">
+              <a class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1" @click="toLogin" v-if="!isLoggedIn" href="/authentication">{{ $t('navigationBar.login') }}</a>
+            </router-link>
+            <a class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1" @click="logout" v-if="isLoggedIn">{{ $t('navigationBar.logout') }}</a>
           </li>
         </ul>
       </div>
@@ -69,10 +78,6 @@ export default {
     const adminAuth = useAdminAuthentication();
     const isLoggedIn = computed(() => userAuth.isLoggedIn.value || adminAuth.isLoggedIn.value);
 
-    function toLogin() {
-      router.push('/authentication')
-    }
-
     function logout() {
       userAuth.logout();
       adminAuth.logout();
@@ -80,7 +85,7 @@ export default {
       router.push('/');
     }
 
-    return { toLogin, logout, isLoggedIn, 'isUserLoggedIn': userAuth.isLoggedIn, 'isAdminLoggedIn': adminAuth.isLoggedIn, 'name': userAuth.name, 'email': userAuth.email }
+    return { logout, isLoggedIn, 'isUserLoggedIn': userAuth.isLoggedIn, 'isAdminLoggedIn': adminAuth.isLoggedIn, 'name': userAuth.name, 'email': userAuth.email }
   }
 }
 </script>
