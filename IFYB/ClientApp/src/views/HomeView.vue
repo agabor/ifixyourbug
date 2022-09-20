90035c0ed2487a64da52f4aeac7d748a4bbc65f8
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-09-20 11:19:53 +0200

    2 phase loading

--- IFYB/ClientApp/src/views/HomeView.vue
+++ IFYB/ClientApp/src/views/HomeView.vue
@@ -1,14 +1,15 @@
 <template>
   <header-component></header-component>
   <features-component></features-component>
-  <name-card></name-card>
-  <pricing-component></pricing-component>
-  <work-with-us></work-with-us>
-  <workflow-components></workflow-components>
-  <footer-component></footer-component>
+  <name-card v-if="loaded"></name-card>
+  <pricing-component v-if="loaded"></pricing-component>
+  <work-with-us v-if="loaded"></work-with-us>
+  <workflow-components v-if="loaded"></workflow-components>
+  <footer-component v-if="loaded"></footer-component>
 </template>
 
 <script>
+  import { ref } from 'vue';
 import FeaturesComponent from '../components/homeComponents/FeaturesComponent.vue';
 import FooterComponent from '../components/homeComponents/FooterComponent.vue';
 import WorkflowComponents from '../components/homeComponents/WorkflowComponents.vue';
@@ -20,5 +21,12 @@ import WorkWithUs from '../components/homeComponents/WorkWithUs.vue';
 export default {
   name: 'HomeView',
   components: { FooterComponent, WorkflowComponents, PricingComponent, WorkWithUs, NameCard, FeaturesComponent, HeaderComponent },
+  setup() {
+    const loaded = ref(false);
+    window.onload = function() {
+      loaded.value = true;
+    }
+    return { loaded }
+  }
 }
 </script>
