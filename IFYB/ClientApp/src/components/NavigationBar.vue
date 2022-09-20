c6e83c5120966db35ef0f8c11979eaec3b02ef74
Lívia Orsós <livia.orsos@codesharp.hu>
2022-09-20 16:03:14 +0200

    #IFYB-170 fix navbar

--- IFYB/ClientApp/src/components/NavigationBar.vue
+++ IFYB/ClientApp/src/components/NavigationBar.vue
@@ -1,17 +1,22 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-white top-0 z-index-3 shadow position-fixed py-2 start-0 end-0 row px-lg-6 px-4">
     <div class="container px-0">
-
       <router-link to="/">
         <a class="navbar-brand font-weight-bolder cursor-pointer ms-sm-3" href="/" >
           {{ $t('navigationBar.projectName') }}
         </a>
       </router-link>
-      <router-link to="/authentication">
-        <a class="btn btn-sm bg-gradient-primary btn-round mb-0 mb-0 ms-auto d-lg-none d-block" href="/authentication"  v-if="!isLoggedIn">{{ $t('navigationBar.login') }}</a>
+      <router-link class="d-lg-none d-block" to="/faq" v-if="!isLoggedIn">
+        <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" href="/faq">
+          {{ $t('navigationBar.faq') }}
+        </a>
+      </router-link>
+      <router-link to="/authentication" class="ms-auto">
+        <a class="btn btn-sm bg-gradient-primary btn-round mb-0 mb-0 ms-auto d-lg-none d-block" href="/authentication" v-if="!isLoggedIn">{{ $t('navigationBar.login') }}</a>
       </router-link>
       <a class="btn btn-sm bg-gradient-primary btn-round mb-0 mb-0 ms-auto d-lg-none d-block" @click="logout" v-if="isLoggedIn">{{ $t('navigationBar.logout') }}</a>
-      <button class="navbar-toggler shadow-none ms-md-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+      
+      <button v-if="isLoggedIn" class="navbar-toggler shadow-none ms-md-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
           <span class="navbar-toggler-bar bar1"></span>
           <span class="navbar-toggler-bar bar2"></span>
@@ -19,7 +24,7 @@
         </span>
       </button>
       <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
-        <ul class="navbar-nav navbar-nav-hover mx-auto" v-if="isAdminLoggedIn">
+        <ul class="navbar-nav navbar-nav-hover me-auto" v-if="isAdminLoggedIn">
           <li class="nav-item dropdown dropdown-hover mx-2">
             <a role="button" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" @click="$router.push('/admin')">
               {{ $t('navigationBar.orders') }}
@@ -56,9 +61,9 @@
         <ul class="navbar-nav d-lg-block d-none">
           <li class="nav-item">
             <router-link to="/authentication">
-              <a class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1" @click="toLogin" v-if="!isLoggedIn" href="/authentication">{{ $t('navigationBar.login') }}</a>
+              <a class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1 ms-auto" @click="toLogin" v-if="!isLoggedIn" href="/authentication">{{ $t('navigationBar.login') }}</a>
             </router-link>
-            <a class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1" @click="logout" v-if="isLoggedIn">{{ $t('navigationBar.logout') }}</a>
+            <a class="btn btn-sm bg-gradient-primary btn-round mb-0 me-1 ms-auto" @click="logout" v-if="isLoggedIn">{{ $t('navigationBar.logout') }}</a>
           </li>
         </ul>
       </div>
@@ -67,16 +72,27 @@
 </template>
 
 <script>
+import { watch } from 'vue';
 import router from '../router';
 import { computed } from "@vue/reactivity";
-import { useUserAuthentication, useAdminAuthentication } from "../store";
+import { useUserAuthentication, useAdminAuthentication, useScripts } from "../store";
 
 export default {
   name: 'NavigationBar',
   setup() {
     const userAuth = useUserAuthentication();
     const adminAuth = useAdminAuthentication();
+    const { loadBootstrap } = useScripts();
     const isLoggedIn = computed(() => userAuth.isLoggedIn.value || adminAuth.isLoggedIn.value);
+
+    if(isLoggedIn.value) {
+      loadBootstrap();
+    }
+    watch(isLoggedIn, () => {
+      if(isLoggedIn.value) {
+        loadBootstrap();
+      }
+    })
 
     function logout() {
       userAuth.logout();
