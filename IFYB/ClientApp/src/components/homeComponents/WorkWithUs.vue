579cea4a09cf70f422bfbcd63c52742b0e42ffbc
Lívia Orsós <livia.orsos@codesharp.hu>
2022-09-20 14:04:12 +0200

    #IFYB-166 add icons

--- IFYB/ClientApp/src/components/homeComponents/WorkWithUs.vue
+++ IFYB/ClientApp/src/components/homeComponents/WorkWithUs.vue
@@ -14,35 +14,47 @@
             </div>
           </div>
         </div>
-        <div class="col-lg-6 justify-content-center flex-column d-flex">
-          <div class="row justify-content-center d-flex mb-3">
+        <div class="col-lg-6 justify-content-center flex-column d-flex my-3">
+          <div class="row justify-content-center d-flex">
             <div class="col-12 d-flex justify-content-center">
-              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom">
-                <span class="fab fa-apple" aria-hidden="true"></span>
+              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom mt-n0 ms-4">
+                <span class="fab fa-ubuntu" aria-hidden="true"></span>
               </div>
             </div>
           </div>
-          <div class="row justify-content-center d-flex my-4">
+          <div class="row justify-content-center d-flex">
             <div class="col-6 d-flex justify-content-center">
-              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom">
-                <span class="fab fa-windows" aria-hidden="true"></span>
+              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom mt-md-0 mb-md-4 ms-md-4 mt-0 mb-4 ms-n6">
+                <span class="fab fa-centos" aria-hidden="true"></span>
               </div>
             </div>
             <div class="col-6 d-flex justify-content-center">
-              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom">
+              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom mt-md-3 ms-md-4 mt-2 ms-7">
                 <span class="fab fa-linux" aria-hidden="true"></span>
               </div>
             </div>
           </div>
-          <div class="row justify-content-center d-flex mt-4 mb-3">
-            <div class="col-md-3 col-4 d-flex justify-content-center">
-              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom">
+          <div class="row justify-content-center d-flex">
+            <div class="col-6 d-flex justify-content-center">
+              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom mt-md-5 ms-md-n5 mt-4 ms-n8">
                 <span class="fab fa-vuejs" aria-hidden="true"></span>
               </div>
             </div>
-            <div class="col-md-3 col-4 d-flex justify-content-center">
-              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom">
+            <div class="col-6 d-flex justify-content-center">
+              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom mt-md-6 ms-md-6 mt-6 ms-7">
                 <span class="fab fa-aws" aria-hidden="true"></span>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center d-flex">
+            <div class="col-6 d-flex justify-content-center">
+              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom mt-md-5 ms-md-7 mt-4 ms-0">
+                <span class="fab fa-chrome" aria-hidden="true"></span>
+              </div>
+            </div>
+            <div class="col-6 d-flex justify-content-center">
+              <div class="d-flex align-items-center justify-content-center bg-white avatar avatar-lg rounded-circle fadeIn3 fadeInBottom mt-md-6 ms-md-n8 mt-5 ms-n6">
+                <span class="fab fa-microsoft" aria-hidden="true"></span>
               </div>
             </div>
           </div>
