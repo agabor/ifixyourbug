9ab48788dd45a4ba5d2603527f515b039aaf65b6
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-09-22 14:48:22 +0200

    create migration

---
+++ IFYB/Migrations/20220922124635_AddCookieModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IFYB.Migrations
+{
+    public partial class AddCookieModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cookies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserAgent = table.Column<string>(type: "text", nullable: false),
+                    Referrer = table.Column<string>(type: "text", nullable: false),
+                    Analytics = table.Column<bool>(type: "boolean", nullable: false),
+                    Advertisement = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cookies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cookies");
+        }
+    }
+}
