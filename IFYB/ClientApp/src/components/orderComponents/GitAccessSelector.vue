0dfcc33a8a07c8eb4586b5523724fdc7b3d260f7
Lívia Orsós <livia.orsos@codesharp.hu>
2022-09-23 16:58:51 +0200

    fix git access selector

--- IFYB/ClientApp/src/components/orderComponents/GitAccessSelector.vue
+++ IFYB/ClientApp/src/components/orderComponents/GitAccessSelector.vue
@@ -2,16 +2,16 @@
   <div class="row">
     <div class="col-12 mb-3">
       <label class="">{{ $t('gitAccessSelector.label') }}</label>
-      <select class="form-control text-black-50" name="choices-git-access" id="choices-git-access" v-model="selectedAccess" @change="$emit('update:modelValue', selectedAccess)">
-        <option :value="{}" selected>{{ $t('gitAccessSelector.placeholder') }}</option>
-        <option :value="access" v-for="(access, idx) in accesses" :key="idx">{{ access.accessMode == 0 ? 'Public repo' : access.accessMode == 1 ? 'Invite' : 'User account' }} - {{ access.url }}</option>
+      <select class="form-control text-black-50" name="choices-git-access" id="choices-git-access" :value="selectedIndex" @change="update($event.target.value)">
+        <option :value="-1" selected>{{ $t('gitAccessSelector.placeholder') }}</option>
+        <option :value="access.id" v-for="(access, idx) in accesses" :key="idx">{{ access.accessMode == 0 ? 'Public repo' : access.accessMode == 1 ? 'Invite' : 'User account' }} - {{ access.url }}</option>
       </select>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed } from 'vue'
 export default {
   name: 'GitAccessSelector',
   emits:['update:modelValue'],
@@ -19,10 +19,21 @@ export default {
     modelValue: Object,
     accesses: Array
   },
-  setup(props) {
-    const selectedAccess = ref(props.modelValue);
+  setup(props, context) {
+    const selectedIndex = computed(() => {
+      return props.modelValue.id ? props.modelValue.id : -1;
+    });
 
-    return { selectedAccess };
+    function update(id) {
+      id = parseInt(id);
+      if(id === -1) {
+        context.emit('update:modelValue', {});
+      } else {
+        context.emit('update:modelValue', props.accesses.find(a => a.id === id));
+      }
+    }
+
+    return { selectedIndex, update };
   }
 }
 </script>
