9cfe93ff7935ed3c177797f89ddae17141d5e4e7
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-09-26 09:33:27 +0200

    fix badges

--- IFYB/ClientApp/src/components/StateBadge.vue
+++ IFYB/ClientApp/src/components/StateBadge.vue
@@ -1,12 +1,12 @@
 <template>
-  <span :class="`${isSimple ? 'badge' : 'text-white bg'}-warning`" v-if="state == 0">{{ $t('orderState.submitted') }}</span>
-  <span :class="`${isSimple ? 'badge' : 'text-white bg'}-info`" v-else-if="state == 1">{{ $t('orderState.confirmed') }}</span>
-  <span :class="`${isSimple ? 'badge' : 'bg'}-light`" v-else-if="state == 2">{{ $t('orderState.rejected') }}</span>
-  <span :class="`${isSimple ? 'badge' : 'bg'}-light`" v-else-if="state == 3">{{ $t('orderState.payed') }}</span>
-  <span :class="`${isSimple ? 'badge' : 'text-white bg'}-success`" v-else-if="state == 4">{{ $t('orderState.completed') }}</span>
-  <span :class="`${isSimple ? 'badge' : 'text-white bg'}-danger`" v-else-if="state == 5">{{ $t('orderState.refundable') }}</span>
-  <span :class="`${isSimple ? 'badge' : 'bg'}-light`" v-else-if="state == 6">{{ $t('orderState.canceled') }}</span>
-  <span :class="`${isSimple ? 'badge' : 'text-white bg'}-dark`" v-else-if="state == 7">{{ $t('orderState.editable') }}</span>
+  <span :class="{'badge-warning': isSimple, 'text-white': !isSimple, 'bg-warning': !isSimple}" v-if="state == 0">{{ $t('orderState.submitted') }}</span>
+  <span :class="{'badge-info': isSimple, 'text-white': !isSimple, 'bg-info': !isSimple}"  v-else-if="state == 1">{{ $t('orderState.confirmed') }}</span>
+  <span :class="{'badge-light': isSimple, 'bg-light': !isSimple}" v-else-if="state == 2">{{ $t('orderState.rejected') }}</span>
+  <span :class="{'badge-light': isSimple, 'bg-light': !isSimple}" v-else-if="state == 3">{{ $t('orderState.payed') }}</span>
+  <span :class="{'badge-success': isSimple, 'text-white': !isSimple, 'bg-success': !isSimple}" v-else-if="state == 4">{{ $t('orderState.completed') }}</span>
+  <span :class="{'badge-danger': isSimple, 'text-white': !isSimple, 'bg-danger': !isSimple}" v-else-if="state == 5">{{ $t('orderState.refundable') }}</span>
+  <span :class="{'badge-light': isSimple, 'bg-light': !isSimple}" v-else-if="state == 6">{{ $t('orderState.canceled') }}</span>
+  <span :class="{'badge-dark': isSimple, 'text-white': !isSimple, 'bg-dark': !isSimple}" v-else-if="state == 7">{{ $t('orderState.editable') }}</span>
 </template>
   
 <script>
