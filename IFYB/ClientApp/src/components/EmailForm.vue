f6c8bc388f360f3dcd6c286aa38c4dec7141d348
Lívia Orsós <livia.orsos@codesharp.hu>
2022-09-27 10:33:36 +0200

    #IFYB-183 fix form-check-input

--- IFYB/ClientApp/src/components/EmailForm.vue
+++ IFYB/ClientApp/src/components/EmailForm.vue
@@ -7,9 +7,11 @@
       <div class="row mb-4">
         <input id="emailInput" class="form-control" ref="userEmailInput" :placeholder="$t('order.emailExample')" type="email" @keyup.enter="trySubmitEmail()" v-model="email" @input="email = $event.target.value.toLowerCase()">
         <div v-if="showPolicy">
-          <div class="form-check form-switch d-flex align-items-center justify-content-center mt-3">
-            <input type="checkbox" class="form-check-input m-0" id="customCheck" :value="acceptedPolicy" @input="$emit('update:acceptedPolicy', !acceptedPolicy)">
-            <label class="custom-control-label m-0 mx-2" for="customCheck">{{ $t('policies.iAcceptAndRead') }}<a class="mx-1 text-decoration-underline" @click="toPrivacyPolicy">{{ $t('policies.privacyPolicy') }}</a></label>
+          <div class="d-flex align-items-center justify-content-center mt-3">
+            <div class="form-check form-switch">
+              <input class="form-check-input" type="checkbox" id="customCheck" :value="acceptedPolicy" @input="$emit('update:acceptedPolicy', !acceptedPolicy)">
+              <label class="form-check-label" for="customCheck">{{ $t('policies.iAcceptAndRead') }}<a class="mx-1 text-decoration-underline" @click="toPrivacyPolicy">{{ $t('policies.privacyPolicy') }}</a></label>
+            </div>
           </div>
           <span class="text-danger" v-if="showRequired"><em><small>{{ $t('policies.requiredPrivacyPolicy') }}</small></em></span>
         </div>
--- IFYB/ClientApp/src/views/ContactForm.vue
+++ IFYB/ClientApp/src/views/ContactForm.vue
@@ -36,9 +36,11 @@
                 <span class="text-danger" v-if="showError && inputErrors.message"><em><small>{{ $t(`${inputErrors.message}`) }}</small></em></span>
               </div>              
               <div v-if="!isLoggedIn" class="align-items-center justify-content-center">
-                <div class="form-check form-switch d-flex align-items-center justify-content-center mt-3">
-                  <input type="checkbox" class="form-check-input m-0" id="customCheck" v-model="acceptedPolicy">
-                  <label class="custom-control-label m-0 mx-2" for="customCheck">{{ $t('policies.iAcceptAndRead') }}<a class="mx-1 text-decoration-underline" @click="toPrivacyPolicy">{{ $t('policies.privacyPolicy') }}</a></label>
+                <div class="d-flex align-items-center justify-content-center mt-3">
+                  <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="customCheck" v-model="acceptedPolicy">
+                    <label class="form-check-label" for="customCheck">{{ $t('policies.iAcceptAndRead') }}<a class="mx-1 text-decoration-underline" @click="toPrivacyPolicy">{{ $t('policies.privacyPolicy') }}</a></label>
+                  </div>
                 </div>
                 <span class="text-danger d-flex justify-content-center" v-if="showError && !acceptedPolicy"><em><small>{{ $t('policies.requiredPrivacyPolicy') }}</small></em></span>
               </div>
