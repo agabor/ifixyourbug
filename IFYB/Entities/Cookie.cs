0fa39dc6516e6a8b913b5ead860d1e062d1accb5
Lívia Orsós <livia.orsos@codesharp.hu>
2022-09-30 13:17:52 +0200

    #IFYB-196 add window.location.search to cookie

--- IFYB/Entities/Cookie.cs
+++ IFYB/Entities/Cookie.cs
@@ -8,18 +8,20 @@ public class Cookie
     public DateTime DateTime { get; set; } = DateTime.UtcNow;
     public string UserAgent { get; set; } = string.Empty;
     public string Referrer { get; set; }
+    public string Search { get; set; }
     public bool Analytics { get; set; }
     public bool Advertisement { get; set; }
 
-     public Cookie(string referrer, bool analytics, bool advertisement)
+     public Cookie(string referrer, string search, bool analytics, bool advertisement)
     {
         Referrer = referrer;
+        Search = search;
         Analytics = analytics;
         Advertisement = advertisement;
     }
 
     public static Cookie FromDto(CookieDto dto)
     {
-        return new Cookie(dto.Referrer, dto.Analytics, dto.Advertisement);
+        return new Cookie(dto.Referrer, dto.Search, dto.Analytics, dto.Advertisement);
     }
 }
--- IFYB/Models/CookieDto.cs
+++ IFYB/Models/CookieDto.cs
@@ -1,13 +1,15 @@
 namespace IFYB.Models;
 public class CookieDto
 {
+    public string Referrer { get; set; }
+    public string Search { get; set; }
     public bool Analytics { get; set; }
     public bool Advertisement { get; set; }
-    public string Referrer { get; set; }
 
-    public CookieDto(string referrer, bool analytics, bool advertisement)
+    public CookieDto(string referrer, string search, bool analytics, bool advertisement)
     {
         Referrer = referrer;
+        Search = search;
         Analytics = analytics;
         Advertisement = advertisement;
     }
