988b6dac05a76c3fd9ab0e12ec5d19a3d5098cc1
Lívia Orsós <livia.orsos@codesharp.hu>
2022-10-13 16:54:21 +0200

    #IFYB-199 fix StackoverflowComponent width

--- IFYB/ClientApp/src/components/homeComponents/StackoverflowCompoent.vue
+++ IFYB/ClientApp/src/components/homeComponents/StackoverflowCompoent.vue
@@ -8,60 +8,56 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-8 col-md-12 mx-auto" v-if="page === 'stackoverflow'">
-          <div class="card shadow-lg d-flex justify-content-center p-4 mt-3">
-            <div class="row">
-              <div class="col-md-6 col-12 pe-md-1">
-                <label>{{ $t('stackoverflow.name') }}</label>
-                <input id="name-input" :class="{'is-invalid': (showError && !!inputErrors.name)}" class="form-control" :placeholder="$t('stackoverflow.name')" type="text" v-model="stackoverflow.name" :disabled="isLoggedIn">
-                <span class="text-danger" v-if="showError && inputErrors.name"><em><small>{{ $t(`${inputErrors.name}`) }}</small></em></span>
-              </div>
-              <div class="col-md-6 col-12 ps-md-1">
-                <label>{{ $t('stackoverflow.email') }}</label>
-                <input type="email" id="email-input" :class="{'is-invalid': (showError && !!inputErrors.email)}" class="form-control" :placeholder="$t('stackoverflow.emailPlaceholder')" v-model="stackoverflow.email" :disabled="isLoggedIn">
-                <span class="text-danger" v-if="showError && inputErrors.email"><em><small>{{ $t(`${inputErrors.email}`) }}</small></em></span>
+        <div class="card shadow-lg d-flex justify-content-center p-4 mt-3" v-if="page === 'stackoverflow'">
+          <div class="row">
+            <div class="col-md-6 col-12 pe-md-1">
+              <label>{{ $t('stackoverflow.name') }}</label>
+              <input id="name-input" :class="{'is-invalid': (showError && !!inputErrors.name)}" class="form-control" :placeholder="$t('stackoverflow.name')" type="text" v-model="stackoverflow.name" :disabled="isLoggedIn">
+              <span class="text-danger" v-if="showError && inputErrors.name"><em><small>{{ $t(`${inputErrors.name}`) }}</small></em></span>
+            </div>
+            <div class="col-md-6 col-12 ps-md-1">
+              <label>{{ $t('stackoverflow.email') }}</label>
+              <input type="email" id="email-input" :class="{'is-invalid': (showError && !!inputErrors.email)}" class="form-control" :placeholder="$t('stackoverflow.emailPlaceholder')" v-model="stackoverflow.email" :disabled="isLoggedIn">
+              <span class="text-danger" v-if="showError && inputErrors.email"><em><small>{{ $t(`${inputErrors.email}`) }}</small></em></span>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <label>{{ $t('stackoverflow.url') }}</label>
+              <input id="url-input" :class="{'is-invalid': (showError && !!inputErrors.name)}" class="form-control" :placeholder="$t('stackoverflow.urlPlaceholder')" type="text" v-model="stackoverflow.url">
+              <span class="text-danger" v-if="showError && inputErrors.url"><em><small>{{ $t(`${inputErrors.url}`) }}</small></em></span>
+            </div>
+          </div>
+          <div class="form-group mb-0 mt-md-0 mt-4">
+            <label>{{ $t('stackoverflow.howCanWeHelp') }}</label>
+            <textarea name="message" :class="{'is-invalid': (showError && !!inputErrors.message)}" class="form-control border-radius-lg" id="message-input" rows="6" :placeholder="$t('stackoverflow.problemDes')" v-model="stackoverflow.message"></textarea>
+            <span class="text-danger" v-if="showError && inputErrors.message"><em><small>{{ $t(`${inputErrors.message}`) }}</small></em></span>
+          </div>              
+          <div v-if="!isLoggedIn" class="align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center mt-3">
+              <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="customCheck" v-model="acceptedPolicy">
+                <label class="form-check-label" for="customCheck">{{ $t('policies.iAcceptAndRead') }}<a class="mx-1 text-decoration-underline" @click="toPrivacyPolicy">{{ $t('policies.privacyPolicy') }}</a></label>
               </div>
             </div>
-            <div class="row">
-              <div class="col-12">
-                <label>{{ $t('stackoverflow.url') }}</label>
-                <input id="url-input" :class="{'is-invalid': (showError && !!inputErrors.name)}" class="form-control" :placeholder="$t('stackoverflow.urlPlaceholder')" type="text" v-model="stackoverflow.url">
-                <span class="text-danger" v-if="showError && inputErrors.url"><em><small>{{ $t(`${inputErrors.url}`) }}</small></em></span>
-              </div>
-            </div>
-            <div class="form-group mb-0 mt-md-0 mt-4">
-              <label>{{ $t('stackoverflow.howCanWeHelp') }}</label>
-              <textarea name="message" :class="{'is-invalid': (showError && !!inputErrors.message)}" class="form-control border-radius-lg" id="message-input" rows="6" :placeholder="$t('stackoverflow.problemDes')" v-model="stackoverflow.message"></textarea>
-              <span class="text-danger" v-if="showError && inputErrors.message"><em><small>{{ $t(`${inputErrors.message}`) }}</small></em></span>
-            </div>              
-            <div v-if="!isLoggedIn" class="align-items-center justify-content-center">
-              <div class="d-flex align-items-center justify-content-center mt-3">
-                <div class="form-check form-switch">
-                  <input class="form-check-input" type="checkbox" id="customCheck" v-model="acceptedPolicy">
-                  <label class="form-check-label" for="customCheck">{{ $t('policies.iAcceptAndRead') }}<a class="mx-1 text-decoration-underline" @click="toPrivacyPolicy">{{ $t('policies.privacyPolicy') }}</a></label>
-                </div>
-              </div>
-              <span class="text-danger d-flex justify-content-center" v-if="showError && !acceptedPolicy"><em><small>{{ $t('policies.requiredPrivacyPolicy') }}</small></em></span>
-            </div>
-            <div class="row">      
-              <div class="col-md-12 d-flex justify-content-center mt-3">
-                <one-click-btn v-model:active="activeBtn" :text="$t('stackoverflow.sendRequest')" class="bg-gradient-primary mx-2" @click="trySubmit()"></one-click-btn>
-              </div>
+            <span class="text-danger d-flex justify-content-center" v-if="showError && !acceptedPolicy"><em><small>{{ $t('policies.requiredPrivacyPolicy') }}</small></em></span>
+          </div>
+          <div class="row">      
+            <div class="col-md-12 d-flex justify-content-center mt-3">
+              <one-click-btn v-model:active="activeBtn" :text="$t('stackoverflow.sendRequest')" class="bg-gradient-primary mx-2" @click="trySubmit()"></one-click-btn>
             </div>
           </div>
         </div>
-        <div class="col-lg-8 col-md-12 mx-auto text-center" v-if="page === 'success'">
-          <div class="card shadow-lg d-flex justify-content-center p-4 mt-3">
-            <div class="info mb-4">
-              <div class="icon icon-shape icon-xl rounded-circle bg-gradient-primary shadow text-center py-3 mx-auto">
-                <i :class="`ni ni-check-bold opacity-10 mt-2`"></i>
-              </div>
+        <div class="card shadow-lg d-flex justify-content-center text-center p-4 mt-3" v-if="page === 'success'">
+          <div class="info mb-4">
+            <div class="icon icon-shape icon-xl rounded-circle bg-gradient-primary shadow text-center py-3 mx-auto">
+              <i :class="`ni ni-check-bold opacity-10 mt-2`"></i>
             </div>
-            <h2>{{ $t('stackoverflowSuccess.title') }}</h2>
-            <p class="mb-4"><span v-html="$t('stackoverflowSuccess.subTitle')"></span></p>
-            <div class="text-center">
-              <button type="button" class="btn bg-gradient-primary my-4 mx-1" @click="newRequest">{{ $t('stackoverflowSuccess.newRequest') }}</button>
-            </div>
+          </div>
+          <h2>{{ $t('stackoverflowSuccess.title') }}</h2>
+          <p class="mb-4"><span v-html="$t('stackoverflowSuccess.subTitle')"></span></p>
+          <div class="text-center">
+            <button type="button" class="btn bg-gradient-primary my-4 mx-1" @click="newRequest">{{ $t('stackoverflowSuccess.newRequest') }}</button>
           </div>
         </div>
       </div>
